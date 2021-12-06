c97f3ac677ab3e6bdea9316dab13d707aafc97fd
unknown <kinchoily@hotmail.com>
2021-12-06 17:35:31 +0000

    Todo.tsx - Pass function down via prop drilling to child component (TodoItem)

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,5 @@
 import Todo from "./models/todo";
-import TodoList from "./TodoItem";
+import TodoItem from "./TodoItem";
 import classes from './Todos.module.css';
 
 // ============================== Notes ==============================
@@ -18,12 +18,19 @@ import classes from './Todos.module.css';
 
 // ===================================================================
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[], onRemoveTodo: (id: string) => void }> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => {
         console.log(item);
-        return <TodoList key={item.id}>{item.text}</TodoList>;
+        return (
+          <TodoItem
+            key={item.id}
+            onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} // onRemove function will receive item.id as a string when its being called 
+          >
+            {item.text}
+          </TodoItem>
+        );
       })}
     </ul>
   );
