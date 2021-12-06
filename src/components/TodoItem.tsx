d34b9072469c38ba94f7dafab27b2a84117c9285
unknown <kinchoily@hotmail.com>
2021-12-06 16:11:57 +0000

    TodoItem - Create and import of styling

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,7 @@
+import classes from './TodoItem.module.css';
+
 const TodoItem: React.FC = (props) => {
-  return <li>{props.children}</li>;
+  return <li className={classes.item}>{props.children}</li>;
 };
 
 export default TodoItem;
